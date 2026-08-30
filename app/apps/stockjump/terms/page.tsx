da0361387950b383134ed5a3aa9e43a6e62d73e0
Nasim Ahmad <khannasimahmad@gmail.com>
2026-08-30 20:46:06 +0530

    refactor(page.tsx): add terms page component

---
+++ app/apps/stockjump/terms/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Terms() {
+    return (
+        <div className="sticky top-0 z-50 border-b border-gray-200 bg-white/90 backdrop-blur-md">
+            <h3>terms and conditions</h3>
+        </div>
+    );
+}
