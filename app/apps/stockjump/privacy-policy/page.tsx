2d1e976eb74c2cc3eebe1c6a203750cb187d7691
Nasim Ahmad <khannasimahmad@gmail.com>
2026-08-30 20:46:02 +0530

    feat(apps/stockjump): add privacy-policy component

---
+++ app/apps/stockjump/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function PrivacyPolicy() {
+    return (
+        <div className="sticky top-0 z-50 border-b border-gray-200 bg-white/90 backdrop-blur-md">
+            <h3>Privacy policy</h3>
+        </div>
+    );
+}
