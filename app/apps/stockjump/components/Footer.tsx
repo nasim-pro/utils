30b9b43c13da9a4218784c33bae7f3858529bb7b
Nasim Ahmad <khannasimahmad@gmail.com>
2026-08-30 20:03:00 +0530

    add footer

---
+++ app/apps/stockjump/components/Footer.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Footer() {
+    return (
+        <footer className="sticky top-0 z-50 border-b border-gray-200 bg-white/90 backdrop-blur-md">
+            <h3>copyright @2026</h3>
+        </footer>
+    );
+}
